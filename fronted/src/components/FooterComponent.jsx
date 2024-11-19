a7a1fe40f3a21f232a5b065c224e2778dc80c811
I'm Groot <80740629+nlmupdy01@users.noreply.github.com>
2024-11-19 13:46:55 +0530

    Update FooterComponent.jsx

--- fronted/src/components/FooterComponent.jsx
+++ fronted/src/components/FooterComponent.jsx
@@ -1,1 +1,26 @@
 
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <footer className = "footer">
+                    <span className="text-muted">
+                        All Rights Reserved 2024-2025
+                         GoLang.com</span>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default FooterComponent
